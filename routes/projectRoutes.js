2677170b37487a442e382db667a6306b41d13896
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2024-08-30 14:20:50 +0700

    routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const projectController = require('../controllers/projectController');
+
+router.post('/projects', projectController.createTask.bind(projectController));
+router.get('/projects', projectController.getAllTask.bind(projectController));
+router.get('/projects/:id', projectController.getTaskById.bind(projectController));
+router.put('/projects/:id', projectController.updateTask.bind(projectController));
+router.delete('/projects/:id', projectController.deleteTask.bind(projectController));
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+router.post('/tasks', taskController.createTask.bind(taskController));
+router.get('/tasks', taskController.getAllTask.bind(taskController));
+router.get('/tasks/:id', taskController.getTaskById.bind(taskController));
+router.put('/tasks/:id', taskController.updateTask.bind(taskController));
+router.delete('/tasks/:id', taskController.deleteTask.bind(taskController));
